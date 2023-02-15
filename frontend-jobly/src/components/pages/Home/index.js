0fb37173a92043b01bb7692e87c095a58db60ee1
sam raphael <samraphael@gmail.com>
2023-02-15 10:53:22 -0500

    homepage

--- frontend-jobly/src/components/pages/Home/index.js
+++ frontend-jobly/src/components/pages/Home/index.js
@@ -1,7 +1,31 @@
 import './Home.css'
+import useGlobalContext from "../../../hooks/state/useGlobalContext";
+import {Link} from "react-router-dom";
 
-const Home= ({...props})=>{
-console.log('home')
-    return <></>
+
+const Home = ({...props}) => {
+    const {user: loggedIn} = useGlobalContext()
+    return <>
+        <CenterMessage>
+            <p id={'CenterMessage_content'}>
+                {loggedIn && 'Welcome back!'}
+                {!loggedIn &&
+                    <>
+                        Welcome to Jobly, Please <Link to={'/login'}>log in</Link> or &nbsp;
+                        <Link to={'/register'}>register</Link>
+                    </>
+                }
+            </p>
+        </CenterMessage>
+
+    </>
 }
 export default Home
+
+
+
+
+const CenterMessage = ({children}) =>
+    <div id={'CenterMessage'}>
+        {children}
+    </div>
